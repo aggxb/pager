afe5ab2604fd399cac68e558969aec1a67c52369
Gabriel Augusto <ag71gabriel@gmail.com>
2026-05-08 17:48:55 -0300

    first commit

---
+++ pager-api/src/main/java/com/pager_api/PagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagerApiApplication.class, args);
+	}
+
+}
